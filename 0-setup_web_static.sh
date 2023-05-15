29fd210492d06741a166ebbc4083a6fcaafbf4b2
55Nights <1905587@students.kca.ac.ke>
2023-05-15 13:05:30 +0300

    setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
-apt-get update
-apt-get -y install nginx
+if ! command -v nginx &> /dev/null; then
+    echo "Installing Nginx..."
+    sudo apt update
+    sudo apt install -y nginx
+    echo "Nginx installed successfully."
+fi
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/current/
 
-echo '<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>' > /data/web_static/releases/test/index.html
+echo "Hello, world!" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chown -hR ubuntu:ubuntu /data/
 
-sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+sudo rm -rf /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-service nginx restart
+sudo chown -hR ubuntu:ubuntu /data/
+
+config_file="/etc/nginx/sites-available/default"
+sudo sed -i "/server_name _;/a \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" $config_file
+sudo service nginx restart
