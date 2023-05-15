b4b2962cbdb6b0e9097c6a8c1d7da4102d1f6ecd
55Nights <1905587@students.kca.ac.ke>
2023-05-15 20:54:16 +0300

    Hello Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+""" Contains a minimam flask Application"""
+
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_world():
+    """Returns A string hello world"""
     return 'Hello, World!'
