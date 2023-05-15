5d969916a305b04f6800dbd6b8a2ba60d318c642
55Nights <1905587@students.kca.ac.ke>
2023-05-15 21:02:24 +0300

    Hello Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_world():
     """Returns A string hello world"""
-    return 'Hello, World!'
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
